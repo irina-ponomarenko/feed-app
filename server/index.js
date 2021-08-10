bd540753f22d5f35ae0b310bede1448c41032b64
irinaponomarenko <ponomarenko02041993@gmail.com>
2021-08-10 18:17:04 +0300

    async/await

--- server/index.js
+++ server/index.js
@@ -9,39 +9,73 @@ app.use(
     bodyParser.json()
 );
 
-app.post('/api/create-new-user', (req, res) => {
+app.post('/api/create-new-user',  async (req, res) => {
     let { name, email, login, password } = req.body;
 
-    Users
-        .findOne({
-           email
-        }, (error, result) => {
-            if(result === null) {
-                Users
-                    .create({
-                        name,
-                        email,
-                        login,
-                        password
-                    },error => {
-                        if (error) {
-                            console.log(error);
-                            return res.json({
-                                status: 502
-                            })
-                        }
-                        return res.json({
-                            status: 200
-                        });
-                    });
-            }
-            else {
-                console.log("Такой пользователь уже есть");
-                return res.json({
-                    message: "*Под этим email уже существует пользователь"
-                });
-            }
+
+    try {
+        const result = await Users.findOne({email});
+        if(result){
+            return res.json({
+                message: "*Под этим email уже существует пользователь"
+            });
+        }
+
+        try {
+            await Users.create({
+                name,
+                email,
+                login,
+                password
+            });
+
+            return res.json({
+                status: 200
+            });
+        } catch (error){
+            console.log(error);
+            return res.json({
+                error: 502
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            error: 502
         });
+    }
+
+   //
+   // Users
+   //      .findOne({
+   //         email
+   //      },  (error, result) => {
+   //          if(result === null) {
+   //              Users
+   //                  .create({
+   //                      name,
+   //                      email,
+   //                      login,
+   //                      password
+   //                  }, error => {
+   //                      if (error) {
+   //                          console.log(error);
+   //                          return res.json({
+   //                              status: 502
+   //                          })
+   //                      }
+   //                      return res.json( {
+   //                          status: 200
+   //                      });
+   //                  });
+   //          }
+   //          else {
+   //              console.log("Такой пользователь уже есть");
+   //              return res.json({
+   //                  message: "*Под этим email уже существует пользователь"
+   //              });
+   //          }
+   //      });
 
 });
 
